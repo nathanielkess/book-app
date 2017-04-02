ab11d61dbbca5d4fd93b0f1ae7b6d1e1a5da0541
Nathaniel <nathaniel@nathanielkessler.com>
2017-04-02 18:25:43 -0400

    show if user is online

--- src/containers/users/users.container.js
+++ src/containers/users/users.container.js
@@ -1,10 +1,22 @@
+import R from 'ramda';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import user from '../../components/users';
+import { compose, renderNothing, branch } from 'recompose';
+import users from '../../components/users';
 import { getUsers } from '../raw-selectors';
+import { getIsLoggedIn } from '../auth/auth.selector';
 
 const mapStateToProps = createStructuredSelector({
   users: getUsers,
+  isLoggedIn: getIsLoggedIn,
 });
 
-export default connect(mapStateToProps, null)(user);
+const showIfAuthorized = branch(
+  props => !R.prop('isLoggedIn', props),
+  renderNothing,
+);
+
+export default compose(
+  connect(mapStateToProps, null),
+  showIfAuthorized,
+)(users);
