0dd22b9288b506163ef9ede459521630a6c246d0
nathanielkess <nathaniel@nathanielkessler.com>
2017-04-30 17:53:14 -0400

    uses the npm prop-types package

--- src/ui/components/book.js
+++ src/ui/components/book.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Book = ({ coverImagePath, title, author, ISBN }) =>
   <div className="book">
